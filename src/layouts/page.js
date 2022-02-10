15dc33939aee4bb54275c4b70ef4298d864400c5
Stackbit Code Editor <projects@stackbit.com>
2022-02-10 22:36:09 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -29,7 +29,7 @@ export class Page extends React.Component {
                                     <img src={withPrefix(image)} alt={imageAlt} />
                                 </div>
                             )}
-                            {markdownContent && }/
+                            {markdownContent && }
                             </article>
                     </div>
                 </div>
